0d01954a8ee0fad8c881d378c3b3f57dad9eaa07
Lukasz <lukasz.kochanek1991@gmail.com>
2015-06-08 23:44:42 +0200

    poprawione z Krakowa

--- medicalclinic/src/main/java/medicalclinic/config/SecurityConfig.java
+++ medicalclinic/src/main/java/medicalclinic/config/SecurityConfig.java
@@ -8,6 +8,8 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CsrfFilter;
+import org.springframework.web.filter.CharacterEncodingFilter;
  
 @Configuration
 @EnableWebSecurity
@@ -29,16 +31,22 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
  
-	  http.authorizeRequests()
-		.antMatchers("/admin/**").access("hasRole('ROLE_ADMIN')")
-		.and()
-		  .formLogin().loginPage("/login").failureUrl("/login?error")
-		  .usernameParameter("login").passwordParameter("password")
-		.and()
-		  .logout().logoutSuccessUrl("/")
-		.and()
-		  .exceptionHandling().accessDeniedPage("/403")
-		.and()
-		  .csrf();
+		CharacterEncodingFilter filter = new CharacterEncodingFilter();
+		  http.authorizeRequests()
+			.antMatchers("/admin/**").access("hasRole('ROLE_ADMIN')")
+			.and()
+			  .formLogin().loginPage("/login").failureUrl("/login?error")
+			  .usernameParameter("login").passwordParameter("password")
+			.and()
+			  .logout().logoutSuccessUrl("/")
+			.and()
+			  .exceptionHandling().accessDeniedPage("/403")
+			.and()
+			  .csrf();
+		  
+		  filter.setEncoding("UTF-8");
+	      filter.setForceEncoding(true);
+	      http.addFilterBefore(filter,CsrfFilter.class);
+
 	}
 }
--- medicalclinic/src/main/java/medicalclinic/config/core/SpringMvcInitializer.java
+++ medicalclinic/src/main/java/medicalclinic/config/core/SpringMvcInitializer.java
@@ -1,7 +1,10 @@
 package medicalclinic.config.core;
 
+import javax.servlet.Filter;
+
 import medicalclinic.config.AppConfig;
 
+import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
  
 public class SpringMvcInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
@@ -20,4 +23,12 @@ public class SpringMvcInitializer extends AbstractAnnotationConfigDispatcherServ
 	protected String[] getServletMappings() {
 		return new String[] { "/" };
 	}
+	 
+	@Override
+	protected Filter[] getServletFilters() {
+
+		CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+		characterEncodingFilter.setEncoding("UTF-8");
+		return new Filter[] { characterEncodingFilter};
+	}	
 }
